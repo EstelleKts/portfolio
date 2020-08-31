99cfc4088adfc3df95d643861d7b6fbf9ce34c48
Estelle NOEL <estelle.noel@gadz.org>
2020-08-31 17:02:16 +0200

    Rework footer layout

--- src/@lekoarts/gatsby-theme-cara/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -10,56 +10,11 @@ const Footer = () => {
 
   return (
     <Box as="footer" variant="footer">
-      <button
-        sx={{
-          variant: `buttons.toggle`,
-          fontWeight: `semibold`,
-          display: `block`,
-          mx: `auto`,
-          mb: 3,
-        }}
-        onClick={toggleColorMode}
-        type="button"
-        aria-label="Toggle dark mode"
-      >
-        {isDark ? `Light` : `Dark`}
-      </button>
-      Copyright &copy; {new Date().getFullYear()}. Made by EstelleKts with 🖤.
-      <br /> All rights reserved.
-      <br />
-      <Flex
-        sx={{
-          justifyContent: `center`,
-          alignItems: `center`,
-          mt: 3,
-          color: `text`,
-          // fontWeight: `semibold`,
-          a: { color: `text` },
-        }}
-      >
-        <img
-          width="30"
-          height="30"
-          src="https://img.lekoarts.de/gatsby/logo_w30.png"
-          alt="LekoArts Logo"
-        />
-        {` `}
-        <Link
-          aria-label="Link to the theme's GitHub repository"
-          sx={{ ml: 2 }}
-          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-cara"
-        >
-          Theme
-        </Link>
-        <div sx={{ mx: 1 }}>by</div>
-        {` `}
-        <Link
-          aria-label="Link to the theme author's website"
-          href="https://www.lekoarts.de/en"
-        >
-          LekoArts
-        </Link>
-      </Flex>
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+      <br /> Made by EstelleKts with 🖤 Theme by{" "}
+      <a href="https://www.lekoarts.de/en/">LekoArts.</a>
+      <br /> Having unusual taste? Switch to{" "}
+      <a onClick={toggleColorMode}> light mode.</a>
     </Box>
   );
 };
