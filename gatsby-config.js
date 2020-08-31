24a9f79d36b95d8a3676ef2d534106a274bdbff6
Estelle NOEL <estelle.noel@gadz.org>
2020-08-31 17:24:04 +0200

    Update banner and SEO meta info

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,15 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Estelle Noël's page`,
+    siteTitleAlt: `Estelle Noël's page`,
+    siteHeadline: `Estelle Noël's page`,
+    siteUrl: `https://estellekts.github.io/portfolio/`,
+    siteDescription: `Welcome to my page, where you can know more about myself.`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@estellekts`,
   },
   plugins: [
     {
